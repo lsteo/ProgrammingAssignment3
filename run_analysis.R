00d6175abe2016658dea43eb73d2b2531120fd7a
Teo Lan See <lansee_teo@yahoo.com>
2014-10-25 14:47:49 +0800

    modified comments

--- run_analysis.R
+++ run_analysis.R
@@ -13,7 +13,7 @@ subject.test <- read.table("./UCI HAR Dataset/test/subject_test.txt", sep = "", 
 X.test <- read.table("./UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE)
 y.test <- read.table("./UCI HAR Dataset/test/y_test.txt", sep="", header = FALSE)
 
-## merge the data in the above 3 data.frame into 1 data.frame - test_data,
+## merge the data in the above 3 data.frame into 1 data.frame - test.data,
 ## col 1 contains subject, col 2 contains labels, col 3 onwards contain data from X_test
 test.data <- cbind(subject.test, y.test, X.test)
 rm(subject.test)
@@ -25,7 +25,7 @@ subject.train <- read.table("./UCI HAR Dataset/train/subject_train.txt", sep = "
 X.train <- read.table("./UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE)
 y.train <- read.table("./UCI HAR Dataset/train/y_train.txt", sep="", header = FALSE)
 
-## merge the data in the above 3 data.frame into 1 data.frame - train_data,
+## merge the data in the above 3 data.frame into 1 data.frame - train.data,
 ## col 1 contains subject, col 2 contains labels, col 3 onwards contain data from X_train
 train.data <- cbind(subject.train, y.train, X.train)
 rm(subject.train)
@@ -56,7 +56,7 @@ rm(merged.data)
 ## read activity_labels.txt into data.frame activity.labels
 activity.labels <- read.table("./UCI HAR Dataset/activity_labels.txt", sep="", header = FALSE)
 
-## replace mean_std_data[, 2] containing labels with its corresponding descriptive activity names in activity_labels[, 2] 
+## replace mean.std.data[, 2] containing labels with its corresponding descriptive activity names in activity_labels[, 2] 
 mean.std.data[,2] <- activity.labels[,2][match(mean.std.data[,2], activity.labels[,1])]
 rm(activity.labels)
 
@@ -89,7 +89,7 @@ rm(mean.std.colnames)
 ## activity and each subject.
 tidy.data <- ddply(mean.std.data, .(subject, activityLabels), numcolwise(mean, na.rm = TRUE))
 
-## export tidy_data to a txt file tidy_data.txt without the row names
+## export tidy.data to a txt file tidy_data.txt without the row names
 write.table(tidy.data, file = "./tidy_data.txt", row.names = FALSE)
 rm(mean.std.data)
 rm(tidy.data)
